4f293e250f1671eb394fecef1133d794a1f19dc5
Abhinav02 <abhibisht769@gmail.com>
2026-03-06 20:48:37 +0530

    Add logging and custom exception handling modules

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys 
+import os
+
+def error_message_detail(error, error_detail:sys) :
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    function_name = exc_tb.tb_frame.f_code.co_name
+
+    return f'ERROR :{str(error)}, FILE :{file_name}, LINE :{line_number}, FUNCTION :{function_name}'
+
+class CustomException(Exception) :
+
+    def __init__(self, error_occurred, error_detail:sys) :
+        super().__init__(error_occurred)
+        self.error_occurred = error_message_detail(error_occurred,error_detail=error_detail)
+
+    def __str__(self) :
+        return self.error_occurred
+    
+   
---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+    
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = os.path.join(LOG_DIR, f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
+
+logging.basicConfig(filename=LOG_FILE, 
+                    level=logging.INFO, 
+                    format="[%(asctime)s] %(levelname)s - %(name)s - %(message)s", 
+                    datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+if __name__ == "__main__" :
+    logging.info("Logging has started .") 
