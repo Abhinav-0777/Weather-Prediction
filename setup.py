2da778a6ef04183d1309a358ef512cb4e4f9af34
Abhinav02 <abhibisht769@gmail.com>
2026-03-31 13:20:53 +0530

    Added artifacts folder for deploying on Render

---
+++ setup.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+series1 = pd.Series([1,2,3,4],
+                    index=['a','b','c','d'])
+
+series2 = pd.Series([1,4,9,16],
+                    index=['a','b','c','d'])
+
+print()
