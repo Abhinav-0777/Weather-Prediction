d44b10ccc760e901d283acc60df571458e33e7f0
Abhinav02 <abhibisht769@gmail.com>
2026-05-02 13:21:22 +0530

    added pytest, linting and caching into the CI workflow

--- src/utils.py
+++ src/utils.py
@@ -42,11 +42,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params) :
 
             model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
-
-            train_model_score = accuracy_score(y_train, y_train_pred)
 
             test_model_score = accuracy_score(y_test, y_test_pred)
 
