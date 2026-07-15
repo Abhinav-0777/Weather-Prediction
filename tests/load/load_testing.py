69b9b6173bdd5201eeadc8508cd093fbd0436f6b
Abhinav02 <abhibisht769@gmail.com>
2026-07-16 01:33:57 +0530

    Added load_testing using Locust with city randomization

---
+++ tests/load/load_testing.py
@@ -0,0 +1,30 @@
+import random
+from locust import HttpUser, constant, task
+
+class MOCK_USER(HttpUser):
+
+    wait_time = constant(0)
+
+    @task
+    def mock_user_journey(self):
+
+        CITIES = [
+                'Albury', 'BadgerysCreek', 'Cobar', 'CoffsHarbour', 'Moree',
+                'Newcastle', 'NorahHead', 'NorfolkIsland', 'Penrith', 'Richmond',
+                'Sydney', 'SydneyAirport', 'WaggaWagga', 'Williamtown',
+                'Wollongong', 'Canberra', 'Tuggeranong', 'MountGinini', 'Ballarat',
+                'Bendigo', 'Sale', 'MelbourneAirport', 'Melbourne', 'Mildura',
+                'Nhil', 'Portland', 'Watsonia', 'Dartmoor', 'Brisbane', 'Cairns',
+                'GoldCoast', 'Townsville', 'Adelaide', 'MountGambier', 'Nuriootpa',
+                'Woomera', 'Albany', 'Witchcliffe', 'PearceRAAF', 'PerthAirport',
+                'Perth', 'SalmonGums', 'Walpole', 'Hobart', 'Launceston',
+                'AliceSprings', 'Darwin', 'Katherine', 'Uluru'
+            ]
+
+        random_city = random.choice(CITIES)
+
+        payload = {
+            'Location': random_city
+        }
+
+        self.client.get('/predict_live', params=payload)
